64d374026b0e05a07703a5efa1162a7dc2073b3d
Braedon Rothert <braedonrothert@gmail.com>
2025-03-16 10:36:24 -0500

    nxbt testing

--- nxbt_bot/rpi_test.py
+++ nxbt_bot/rpi_test.py
@@ -1,34 +1,41 @@
 # Handles trading errors and retries 
-try: 
-    import nxbt
-except ImportError:
-    print("nxbt module not found.")
+import nxbt
 
 # Start the NXBT service
 try: 
     nx = nxbt.Nxbt()
 except Exception:
-    print("Failed to start NXBT service.")
+    print(f"Error initializing Nxbt: {Exception}")
 else:
-    print("NXBT service started.")
-    
+    print("Nxbt initialized successfully")
+
 # Get a list of all previously connected Switches and pass it as a reconnect_address argument
-controller_index = nx.create_controller(
-    nxbt.PRO_CONTROLLER,
-    reconnect_address=nx.get_switch_addresses())
 try: 
+    controller_index = nx.create_controller(nxbt.PRO_CONTROLLER, reconnect_address=nx.get_switch_addresses())
     nx.wait_for_connection(controller_index)
 except OSError:
     print(f"Connection OSError: {OSError}")
 except Exception as e:
     print(f"Connection Error: {e}")
+else: 
+    print("Connected to Switch")
 
 # Press the B button
 # press_buttons defaults to pressing a button for 0.1s and releasing for 0.1s
-nx.press_buttons(controller_index, [nxbt.Buttons.B])
+try: 
+    nx.press_buttons(controller_index, [nxbt.Buttons.A])
+except Exception:
+    print(f"Press Buttons Error: {Exception}")
+else:
+    print("Pressed A button")
 
 # Pressing the B button for 1.0s instead of 0.1s
-nx.press_buttons(controller_index, [nxbt.Buttons.B], down=1.0)
+try: 
+    nx.press_buttons(controller_index, [nxbt.Buttons.B])
+except Exception:
+    print(f"Press Buttons Error: {Exception}")
+else:
+    print("Pressed B button")
 
 # This frees up the adapter that was in use by this controller
 nx.remove_controller(controller_index)
