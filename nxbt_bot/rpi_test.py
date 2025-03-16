97dcf1fc6a102026163d42382cc32feab3714806
Braedon Rothert <braedonrothert@gmail.com>
2025-03-15 22:47:32 -0500

    nxbt testing on rpi

--- nxbt_bot/rpi_test.py
+++ nxbt_bot/rpi_test.py
@@ -4,8 +4,10 @@ import nxbt
 # Start the NXBT service
 nx = nxbt.Nxbt()
 
-# Create a Pro Controller and wait for it to connect
-controller_index = nx.create_controller(nxbt.PRO_CONTROLLER)
+# Get a list of all previously connected Switches and pass it as a reconnect_address argument
+controller_index = nx.create_controller(
+    nxbt.PRO_CONTROLLER,
+    reconnect_address=nx.get_switch_addresses())
 nx.wait_for_connection(controller_index)
 
 # Press the B button
@@ -15,4 +17,5 @@ nx.press_buttons(controller_index, [nxbt.Buttons.B])
 # Pressing the B button for 1.0s instead of 0.1s
 nx.press_buttons(controller_index, [nxbt.Buttons.B], down=1.0)
 
-print("Connected")
+# This frees up the adapter that was in use by this controller
+nx.remove_controller(controller_index)
