87fc1daa46bfff2f31344bf4af76913679fd1335
unknownsoup <braedonrothert@gmail.com>
2025-03-16 20:16:35 -0500

    working nxbt script from start to set link code screen

--- nxbt_bot/rpi_test.py
+++ nxbt_bot/rpi_test.py
@@ -3,11 +3,142 @@ import time
 from random import randint
 
 import nxbt
+from nxbt import PRO_CONTROLLER
 from nxbt import Buttons
 from nxbt import Sticks
 
+"""
+FIRST GOAL: Go from 'Change Grip/Order screen to Enter Trade Code'
+
+
+B 
+B              
+stick up
+stick left
+stick left
+A              Select Game
+A              Select User
+23 s  seconds to start screen
+A
+26 s  seconds to to load game
+X              Open main menu
+stick right    Go over to from party pokemon to menu
+stick down     Go down to Poke Portal
+stick down
+stick down
+A              Select poke portal
+2 second delay
+stick down     Go down to link trade
+stick down
+A              Select link trade
+stick down     begin searching -> set link code
+A              SET LINK CODE SCREEN
+
+
+"""
+
+wait = 1
+
+def pressA():
+    try: 
+        nx.press_buttons(index, [nxbt.Buttons.A])
+        time.sleep(wait)
+    except Exception:
+        print(f"Press Buttons Error: {Exception}")
+    else:
+        print("Pressed A button")
+
+def pressB():
+    try: 
+        nx.press_buttons(index, [nxbt.Buttons.B])
+        time.sleep(wait)
+    except Exception:
+        print(f"Press Buttons Error: {Exception}")
+    else:
+        print("Pressed B button")
+
+def pressX():
+    try: 
+        nx.press_buttons(index, [nxbt.Buttons.X])
+        time.sleep(wait)
+    except Exception:
+        print(f"Press Buttons Error: {Exception}")
+    else:
+        print("Pressed X button")
+
+def pressY():
+    try: 
+        nx.press_buttons(index, [nxbt.Buttons.Y])
+        time.sleep(wait)
+    except Exception:
+        print(f"Press Buttons Error: {Exception}")
+    else:
+        print("Pressed Y button")
+
+def directionUP():
+    try: 
+        nx.press_buttons(index, [nxbt.Buttons.DPAD_UP])
+        time.sleep(wait)
+    except Exception:
+        print(f"Press Buttons Error: {Exception}")
+    else:
+        print("Pressed UP button")
+
+def directionDOWN():
+    try: 
+        nx.press_buttons(index, [nxbt.Buttons.DPAD_DOWN])
+        time.sleep(wait)
+    except Exception:
+        print(f"Press Buttons Error: {Exception}")
+    else:
+        print("Pressed DOWN button")
+
+def directionLEFT():
+    try: 
+        nx.press_buttons(index, [nxbt.Buttons.DPAD_LEFT])
+        time.sleep(wait)
+    except Exception:
+        print(f"Press Buttons Error: {Exception}")
+    else:
+        print("Pressed LEFT button")
+
+def directionRIGHT():
+    try: 
+        nx.press_buttons(index, [nxbt.Buttons.DPAD_RIGHT])
+        time.sleep(wait)
+    except Exception:
+        print(f"Press Buttons Error: {Exception}")
+    else:
+        print("Pressed RIGHT button")
+
+
 def random_colour():
     return [randint(0, 255),randint(0, 255),randint(0, 255),]
+
+def changegrip_to_setlinkcode():
+    pressB()
+    pressB()
+    directionUP()
+    directionLEFT()
+    directionLEFT()
+    pressA()
+    pressA()
+    time.sleep(23)
+    pressA()
+    time.sleep(30)
+    pressX()
+    directionRIGHT()
+    directionDOWN()
+    directionDOWN()
+    directionDOWN()
+    pressA()
+    time.sleep(2)
+    directionDOWN()
+    directionDOWN()
+    pressA()
+    directionDOWN()
+    pressA()
+    print("SUCCESS... now exiting")
 
 # Start the NXBT service
 try: 
@@ -17,50 +148,19 @@ except Exception:
 else:
     print("Nxbt initialized successfully")
 
-# Get a list of all previously connected Switches and pass it as a reconnect_address argument
-# Get a list of all available Bluetooth adapters
-adapters = nx.get_available_adapters()
-# Prepare a list to store the indexes of the
-# created controllers.
-controller_idxs = []
-# Loop over all Bluetooth adapters and create
-# Switch Pro Controllers
-try: 
-    for i in range(0, len(adapters)):
-        index = nx.create_controller(
-            nxbt.PRO_CONTROLLER,
-            adapter_path=adapters[i],
-            colour_body=random_colour(),
-            colour_buttons=random_colour())
-        controller_idxs.append(index)
-
-        # Select the last controller for input
-        controller_index = controller_idxs[-1]
-
-        # Wait for the switch to connect to the controller
-        nx.wait_for_connection(controller_index)
-except Exception:
-    print(f"Error creating controller: {Exception}")
-else:
-    print("Controller created successfully")
-
-# Press the B button
-# press_buttons defaults to pressing a button for 0.1s and releasing for 0.1s
-try: 
-    nx.press_buttons(controller_index, [nxbt.Buttons.A])
-except Exception:
-    print(f"Press Buttons Error: {Exception}")
-else:
-    print("Pressed A button")
-
-time.sleep(1.0)
 
 try: 
-    nx.press_buttons(controller_index, [nxbt.Buttons.B])
+    index = nx.create_controller(PRO_CONTROLLER)
 except Exception:
-    print(f"Press Buttons Error: {Exception}")
+    print("Couldn't create controller")
 else:
-    print("Pressed B button")
+    print("Controller created!")
 
-# Clears all macros from a given controller
-nx.clear_macros(controller_index)
+try: 
+    nx.wait_for_connection(index)
+except Exception:
+    print("Couldn't connect controller")
+else:
+    print("Controller connected!")
+
+changegrip_to_setlinkcode()
